4b49821ed2215344bde0b572549e648950c585ea
Daniel <daniel.silva@solinftec.com.br>
2019-03-18 16:11:52 -0300

    teste recebendo menssagem

--- src/Chromecast/InitCast.js
+++ src/Chromecast/InitCast.js
@@ -5,12 +5,14 @@ function initCast(){
 
   options.customNamespaces = Object.assign({});
   options.customNamespaces[CHANNEL] = cast.framework.system.MessageType.JSON;
+
   ctx.addCustomMessageListener(CHANNEL, function(customEvent) {
     var message = customEvent.data
     console.log("Message received from " + 
                 "[" +  customEvent.senderId +  "] " +
                 ": " + message);
     document.getElementById("main").innerHTML = message;
+    setTimeout(()=> console.log('messages do bus: ', message), 500);
   });
 
   ctx.start(options);
