74a1f2715cbd775e1cd8c7fcbeea95ca99b6215a
Daniel <daniel.silva@solinftec.com.br>
2019-03-18 13:45:49 -0300

    SDK receceiver 2.0

---
+++ src/App.js
@@ -0,0 +1,8 @@
+displayText = (text) => 
+{
+    //document.getElementById('main').innerHTML = text;
+    //var obj = JSON.parse(text);
+    //executeAction({actionId: obj.actionId, data: obj.data});
+
+    window.castReceiverManager.setApplicationState(obj);
+};
