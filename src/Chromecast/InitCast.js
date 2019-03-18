54f02ee1019e317d6ed496024a3809855b2e64b8
Daniel <daniel.silva@solinftec.com.br>
2019-03-18 17:02:52 -0300

    envio de messages

--- src/Chromecast/InitCast.js
+++ src/Chromecast/InitCast.js
@@ -1,3 +1,12 @@
 function initCast(){
-  cast.framework.CastReceiverContext.getInstance().start();
+  var ctx = cast.framework.CastReceiverContext.getInstance();
+  var options = new cast.framework.CastReceiverOptions();
+  var CHANNEL = 'urn:x-cast:com.solinftec.alice';
+
+  ctx.sendCustomMessage(CHANNEL, {
+    type: 'status',
+    message: 'aehehehheheheh'
+  });
+
+  ctx.start(options);
 };
