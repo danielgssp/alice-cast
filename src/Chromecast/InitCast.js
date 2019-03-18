71fa9e28c9110fe2c0000d3c0e08df1fa59e898e
Daniel <daniel.silva@solinftec.com.br>
2019-03-18 16:57:27 -0300

    recebendo messages

--- src/Chromecast/InitCast.js
+++ src/Chromecast/InitCast.js
@@ -1,18 +1,3 @@
 function initCast(){
-  var ctx = cast.framework.CastReceiverContext.getInstance();
-  var options = new cast.framework.CastReceiverOptions();
-  var CHANNEL = 'urn:x-cast:com.solinftec.alice';
-
-  options.customNamespaces = Object.assign({});
-  options.customNamespaces[CHANNEL] = cast.framework.system.MessageType.JSON;
-  
-  ctx.addCustomMessageListener(CHANNEL, function(customEvent) {
-    var message = JSON.stringify(customEvent.data)
-    console.log("Message received from " + 
-                "[" +  customEvent.senderId +  "] " +
-                ": " + message);
-    document.getElementById("main").innerHTML = message;
-  });
-
-  ctx.start(options);
+  cast.framework.CastReceiverContext.getInstance().start();
 };
