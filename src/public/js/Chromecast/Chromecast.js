7bd96290dc01378dfa092f53f47ad1da8f43b865
Daniel <daniel.silva@solinftec.com.br>
2019-03-20 11:59:03 -0300

    Custom Receiver [CAF]

--- src/public/js/Chromecast/Chromecast.js
+++ src/public/js/Chromecast/Chromecast.js
@@ -1,17 +1,16 @@
-chromecast = async() =>
+chromecast = () =>
 {
-  debugger;
-  const context = await cast.framework.CastReceiverContext.getInstance()
-  const CHANNEL = 'urn:x-cast:com.solinftec.alice'
+  const context = cast.framework.CastReceiverContext.getInstance();
+  const CHANNEL = 'urn:x-cast:com.solinftec.alice';
 
   context.addCustomMessageListener(CHANNEL, customEvent => 
   {
-    console.log(customEvent.data)
+    console.log(customEvent.data);
     document.getElementById("main").innerHTML = customEvent.data.msg;
 
     context.sendCustomMessage(CHANNEL, customEvent.senderId, 
     {
-      message: 'Hello',
+      message: 'Hello'
     });
   });
 
