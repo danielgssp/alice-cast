09021a125e382d1807c9398f65624dc7b6e54de1
danielgssp <daniel.silva@solinftec.com.br>
2019-03-21 08:36:25 -0300

    implantando fluxo do app

--- src/public/js/App.js
+++ src/public/js/App.js
@@ -1,4 +1,4 @@
-class Utils 
+class App 
 {
     buildDOM = () =>
     {
--- src/public/js/Chromecast.js
+++ src/public/js/Chromecast.js
@@ -2,13 +2,13 @@ class Chromecast
 {
   constructor ()
   {
-    this.utils = new Utils();
+    this.app = new App();
     this.context = cast.framework.CastReceiverContext.getInstance();
     this.CHANNEL = 'urn:x-cast:com.solinftec.alice';
 
     this.context.addCustomMessageListener(this.CHANNEL, customEvent => 
     {
-      this.utils.managerData(customEvent.data);
+      this.app.managerData(customEvent.data);
     });
   
     this.context.start();
