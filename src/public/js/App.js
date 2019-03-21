8832daf76f8ae0f374e9d8c555526972e420f141
danielgssp <daniel.silva@solinftec.com.br>
2019-03-21 08:38:47 -0300

    teste do fluxo

--- src/public/js/App.js
+++ src/public/js/App.js
@@ -1,19 +1,19 @@
 class App 
 {
-    buildDOM = () =>
-    {
-        let start;
-        const url = 'http://172.16.0.169:4212/connection/open';
+    // buildDOM = () =>
+    // {
+    //     let start;
+    //     const url = 'http://172.16.0.169:4212/connection/open';
 
-        fetch(url).then(r =>r.json()).then(j => 
-        {
-            console.log(j);
-            document.getElementById("main").innerHTML = j.page;
-            start = new Function(`return ${j.func}`)();
-        }).catch(e => console.log(e));    
+    //     fetch(url).then(r =>r.json()).then(j => 
+    //     {
+    //         console.log(j);
+    //         document.getElementById("main").innerHTML = j.page;
+    //         start = new Function(`return ${j.func}`)();
+    //     }).catch(e => console.log(e));    
         
-        start();
-    };
+    //     start();
+    // };
 
     managerData = (data) =>
     {
