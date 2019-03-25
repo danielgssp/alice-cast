243c40679c09cddc12df4f861d6a4afe3d79bcaf
André Gallacci <andre.gallacci@gmail.com>
2019-03-25 14:40:36 -0300

    Teste implementação em um só arquivo

--- src/public/js/Chromecast.js
+++ src/public/js/Chromecast.js
@@ -1,21 +1,17 @@
-class Chromecast
-{
-  constructor ()
-  {
+class Chromecast {
+  constructor() {
     this.app = new App();
     this.context = cast.framework.CastReceiverContext.getInstance();
     this.CHANNEL = 'urn:x-cast:com.solinftec.alice';
 
-    this.context.addCustomMessageListener(this.CHANNEL, customEvent => 
-    {
+    this.context.addCustomMessageListener(this.CHANNEL, customEvent => {
       this.app.managerData(customEvent.data);
 
-      this.context.sendCustomMessage(this.CHANNEL, customEvent.senderId, 
-      {
+      this.context.sendCustomMessage(this.CHANNEL, customEvent.senderId, {
         message: 'Started Session'
       });
     });
-  
+
     this.context.start();
   };
 };
